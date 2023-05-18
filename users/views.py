66133a330ce948b6b3544b9d87192601a4011a00
Mblshko <mishka-007@yandex.ru>
2023-05-19 01:07:40 +0300

    Сортировка пользователей по кол-ву постов

--- users/views.py
+++ users/views.py
@@ -1,14 +1,19 @@
 from django.contrib.auth.models import User
+from django.db.models import Count
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+
 
 from .serializers import UserSerializer
 
 
 class UserList(generics.ListAPIView):
-    queryset = User.objects.all()
+    queryset = User.objects.annotate(cnt=Count('articles')).order_by('-cnt')
     serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
